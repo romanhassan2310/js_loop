f9792b53e6375aa5a2241f315c9baf6976b59042
Roman <romanhassan328@gmail.com>
2025-08-19 22:52:09 +0600

    added while.js

---
+++ while.js
@@ -0,0 +1,16 @@
+const number = 7;
+if (number < 10){
+    console.log('I give a small number.');
+}
+console.log('something new is cooking');
+
+
+// -----------WHILE---LOOP---------------
+
+let num = 0;
+while (num < 5){
+    console.log('looooping', num);
+    // num = num + 1;
+    // num+=1;
+    num++;
+}
