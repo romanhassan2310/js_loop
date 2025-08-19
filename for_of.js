a7e1af590669beb0c3ef22aaa1e8d58c241115cf
Roman <romanhassan328@gmail.com>
2025-08-19 22:20:13 +0600

    first commit

---
+++ for_of.js
@@ -0,0 +1,18 @@
+const numbers = [12, 56, 67, 89, 45, 99, 100];
+
+console.log(numbers);
+
+// for (const item of numbers){
+//     console.log(item)
+// }
+
+const fruits = ['orange.', 'apple.', 'banana.', 'jackfruit.', 'watermelon.'];
+
+// for (){
+
+// }
+
+for (const fruit of fruits){
+    console.log(fruit);
+    console.log('I want to eat.')
+}
---
+++ recap.js
@@ -0,0 +1,17 @@
+// const: value will not change.
+// let: value can change.
+// var: we should not use it.
+
+// variable 
+const amount = 200;
+
+// condition
+if (amount > 100){
+    console.log('You are rich man.');
+}
+else {
+    console.log("You are poor person");
+}
+
+// array
+const numbers = [12, 45, 50, 54];
