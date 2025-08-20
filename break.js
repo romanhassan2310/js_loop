51b7fc092d56af88a866e563cfcc6de709ef8489
Roman <romanhassan328@gmail.com>
2025-08-20 23:13:26 +0600

    added final while-loop

---
+++ break.js
@@ -0,0 +1,17 @@
+for (let i = 0; i < 150; i++){
+    console.log(i);
+    if ( i >= 10){
+        break;
+    }
+}
+console.log('Life after break up');
+
+let n =55;
+while(n > 25){
+    console.log(n);
+    if(n<50){
+        break;
+    }
+    n--;
+    
+}
---
+++ do-while.js
@@ -0,0 +1,18 @@
+// almost not use.
+
+// let i = 0;
+// while ( i <5){
+//     console.log(i);
+//     i++;
+// }
+
+// let n=0;
+// do{
+//     console.log(n);
+//     n++;
+// }
+// while(n<5)
+
+for (let i = 0; i < 5; i+=3){
+    console.log(i);
+}
